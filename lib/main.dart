3939f72e724a4c658604901f782a96d10d159595
iulvimercan <ibrahimulvimercan@gmail.com>
2024-07-04 17:34:26 +0300

    container & styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp( MaterialApp(
     home: Scaffold(
-      body: Text('Hello World!')
-    )
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.bottomRight,
+              end: Alignment.topLeft,
+              colors: [
+                Colors.white,
+                Color.fromARGB(255, 176, 216, 255),
+              ]
+          ),
+        ),
+        child: const Center(
+          child: Text(
+            'Hello, World!',
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 28,
+            ),
+          ),
+        )
+      ),
+    ),
   ));
 }
