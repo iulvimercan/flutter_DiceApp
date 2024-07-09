fffb8a974147e1330f732247e79cb3863a17ef30
iulvimercan <ibrahimulvimercan@gmail.com>
2024-07-09 17:55:02 +0300

    statefulWidget for rollDice

---
+++ lib/dice_roller.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+
+}
+
+
+
+class _DiceRollerState extends State<DiceRoller> {
+
+  String activeDice = 'assets/images/dice-1.png';
+
+  void rollDice() {
+    setState(() {
+      activeDice = 'assets/images/dice-2.png';
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          activeDice,
+          width: 200,
+        ),
+        const SizedBox(height: 50),
+        TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: const Color.fromARGB(255, 57, 173, 255),
+                padding: const EdgeInsets.all(21.0),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0)
+                ),
+                textStyle: const TextStyle(
+                    fontSize: 21
+                )
+            ),
+            child: const Text('Roll Dice')),
+
+      ],
+    );
+  }
+}
--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,3 +1,4 @@
+import 'package:dice_app/dice_roller.dart';
 import 'package:flutter/material.dart';
 
 class GradientContainer extends StatelessWidget{
@@ -15,32 +16,8 @@ class GradientContainer extends StatelessWidget{
               colors: colors
           ),
         ),
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                'assets/images/dice-1.png',
-                width: 200,
-              ),
-              const SizedBox(height: 50),
-              TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                      foregroundColor: Colors.white,
-                      backgroundColor: const Color.fromARGB(255, 57, 173, 255),
-                      padding: const EdgeInsets.all(21.0),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12.0)
-                      ),
-                      textStyle: const TextStyle(
-                          fontSize: 21
-                      )
-                  ),
-                  child: const Text('Roll Dice')),
-
-            ],
-          ),
+        child: const Center(
+          child: DiceRoller(),
         )
     );
   }
