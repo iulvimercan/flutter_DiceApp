080a16e0c4cb2b5f76f87193ac1f00c3da58d925
iulvimercan <ibrahimulvimercan@gmail.com>
2024-07-09 14:55:44 +0300

    required named parameters

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,19 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:dice_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget{
-  const GradientContainer({super.key});
+  const GradientContainer({super.key, required this.colors});
+
+  final List<Color> colors;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           gradient: LinearGradient(
               begin: Alignment.bottomRight,
               end: Alignment.topLeft,
-              colors: [
-                Colors.white,
-                Color.fromARGB(255, 176, 216, 255),
-              ]
+              colors: colors
           ),
         ),
         child: const Center(
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,12 @@ import 'package:dice_app/gradient_container.dart';
 void main() {
   runApp( const MaterialApp(
     home: Scaffold(
-      body: GradientContainer(),
+      body: GradientContainer(
+          colors: [
+            Colors.white,
+            Color.fromARGB(255, 176, 216, 255)
+          ],
+      ),
     ),
   ));
 }
